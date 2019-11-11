319576ba5f779d21ee496bd64dbfe6cee2fe12a1
YeshaDave <yeshadave9@gmail.com>
2019-11-10 22:46:31 -0800

    user-feed Frontend

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -2,6 +2,14 @@ import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
 import Signup from './signup/userSignup'
 import Launchpage from './users/launchpage'
+import UserHome from './userFeed/userHome'
+import Explore from './userFeed/explore'
+import Messages from './userFeed/messages'
+import Bookmarks from './userFeed/bookmarks'
+import Lists from './userFeed/lists'
+import Profile from './userFeed/profile'
+import Analytics from './userFeed/analytics'
+import Settings from './userFeed/settings'
 
 class Main extends Component {
     render(){
@@ -9,6 +17,14 @@ class Main extends Component {
             <div>
             <Route path="/signup" component={Signup}/>
             <Route path="/launchpage" component={Launchpage}/>
+            <Route path="/home" component={UserHome}/>
+            <Route path="/explore" component={Explore}/>
+            <Route path="/messages" component={Messages}/>
+            <Route path="/bookmarks" component={Bookmarks}/>
+            <Route path="/lists" component={Lists}/>
+            <Route path="/profile" component={Profile}/>
+            <Route path="/analytics" component={Analytics}/>
+            <Route path="/settings" component={Settings}/>
             </div>
             )
     }
