1a40390d1c73940ecfba722143951b1256416090
Harshada Jivane <harshadajivane@Vireshs-MacBook-Pro.local>
2019-11-15 02:17:00 -0800

    Added viewing members and subscribers of any list API

--- backend/DataAccessLayer/lists.js
+++ backend/DataAccessLayer/lists.js
@@ -1,43 +1,60 @@
-const { lists } = require('./Models/ListSchema');
-const tableName = 'tweets';
+const { lists } = require("./Models/ListSchema");
+const tableName = "tweets";
 
-const getLists = connection => (list= {}) => {
-    return new Promise((resolve, reject) => {
-        lists.find(list, function (err, docs) {
-            return err ? reject(err) : resolve(docs);
-        });
+const getLists = connection => (list = {}) => {
+  return new Promise((resolve, reject) => {
+    lists.find(list, function(err, docs) {
+      return err ? reject(err) : resolve(docs);
     });
+  });
 };
 
-const saveLists = connection => (list) => {
-    let listsRec= new lists(list);
-    return new Promise((resolve, reject) => {
-        listsRec.save(function (err, docs) {
-            return err ? reject(err) : resolve(docs);
-        })
+const saveLists = connection => list => {
+  let listsRec = new lists(list);
+  return new Promise((resolve, reject) => {
+    listsRec.save(function(err, docs) {
+      return err ? reject(err) : resolve(docs);
     });
+  });
 };
 
-const getMemberships = connection => (list= {}) => {
-    return new Promise((resolve, reject) => {
-        lists.find(list, function (err, docs) {
-            return err ? reject(err) : resolve(docs);
-        });
+const getMemberships = connection => (list = {}) => {
+  return new Promise((resolve, reject) => {
+    lists.find(list, function(err, docs) {
+      return err ? reject(err) : resolve(docs);
     });
+  });
 };
 
-const getSubscriptions = connection => (list= {}) => {
-    return new Promise((resolve, reject) => {
-        lists.find(list, function (err, docs) {
-            return err ? reject(err) : resolve(docs);
-        });
+const getSubscriptions = connection => (list = {}) => {
+  return new Promise((resolve, reject) => {
+    lists.find(list, function(err, docs) {
+      return err ? reject(err) : resolve(docs);
     });
+  });
 };
 
+const getMembers = connection => (list = {}) => {
+  return new Promise((resolve, reject) => {
+    lists.find(list, function(err, docs) {
+      return err ? reject(err) : resolve(docs);
+    });
+  });
+};
+
+const getSubscribers = connection => (list = {}) => {
+  return new Promise((resolve, reject) => {
+    lists.find(list, function(err, docs) {
+      return err ? reject(err) : resolve(docs);
+    });
+  });
+};
 
 module.exports = {
-    getLists,
-    saveLists,
-    getMemberships,
-    getSubscriptions
+  getLists,
+  saveLists,
+  getMemberships,
+  getSubscriptions,
+  getMembers,
+  getSubscribers
 };
--- backend/routes/lists.js
+++ backend/routes/lists.js
@@ -1,86 +1,112 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const uuidv4 = require('uuid/v4');
-var passport = require('passport');
-const multer = require('multer');
-const path = require('path');
+const uuidv4 = require("uuid/v4");
+var passport = require("passport");
+const multer = require("multer");
+const path = require("path");
 
-const upload = multer({ dest: path.join(__dirname, '..', 'uploads/') });
-const { getLists, saveLists, getMemberships,getSubscriptions } = require('../DataAccessLayer');
+const upload = multer({ dest: path.join(__dirname, "..", "uploads/") });
+const {
+  getLists,
+  saveLists,
+  getMemberships,
+  getSubscriptions,
+  getMembers,
+  getSubscribers
+} = require("../DataAccessLayer");
 
 // Set up middleware
-var requireAuth = passport.authenticate('jwt', { session: false });
-
-router.get('/', async function (req, res, next) {
-    try {
-      
-      res.json("Lists");
-    }
-    catch (e) {
-      res.status(500).send(e.message || e);
-    }
-  });
+var requireAuth = passport.authenticate("jwt", { session: false });
 
 //Get the created lists
-router.get('/list', requireAuth, async function (req, res, next) {
-   
-    try {
-        const loggedInUser = req.user;
-      
-        const list = {
-            ownerID:  loggedInUser.userID
-        };
-        results = await getLists(list);
-        res.json(results);
-    } catch (e) {
-        res.status(500).send(e.message || e);
-    }
+router.get("/", requireAuth, async function(req, res, next) {
+  try {
+    const loggedInUser = req.user;
+
+    const list = {
+      ownerID: loggedInUser.userID
+    };
+    results = await getLists(list);
+    res.json(results);
+  } catch (e) {
+    res.status(500).send(e.message || e);
+  }
 });
 
 //Create a list
-router.post('/create', requireAuth, async function (req, res, next) {
-    const { listName,listDesc,isPrivate } = req.body;
-    try {
-      const loggedInUser = req.user;
-     const list = {
-        ownerID: loggedInUser.userID,
-        listName:listName,
-        listDesc:listDesc,
-        isPrivate:isPrivate
-      };
-      await saveLists(list);
-      res.json("List Created");
-    } catch (e) {
-      res.status(500).send(e.message || e);
-    }
-  });
+router.post("/create", requireAuth, async function(req, res, next) {
+  const { listName, listDesc, isPrivate } = req.body;
+  try {
+    const loggedInUser = req.user;
+    const list = {
+      ownerID: loggedInUser.userID,
+      listName: listName,
+      listDesc: listDesc,
+      isPrivate: isPrivate
+    };
+    await saveLists(list);
+    res.json("List Created");
+  } catch (e) {
+    res.status(500).send(e.message || e);
+  }
+});
 
-  router.get('/memberships', requireAuth, async function (req, res, next) {
-   
+//Gets names of lists user is a member of
+
+router.get("/memberships", requireAuth, async function(req, res, next) {
+  try {
+    const loggedInUser = req.user;
+    const list = {
+      members: loggedInUser.userID
+    };
+    const results = await getMemberships(list);
+    res.json(results);
+  } catch (e) {
+    res.status(500).send(e.message || e);
+  }
+});
+
+//Gets names of lists user has subscribed to
+
+router.get("/subscriptions", requireAuth, async function(req, res, next) {
+  try {
+    const loggedInUser = req.user;
+    const list = {
+      subscribers: loggedInUser.userID
+    };
+    const results = await getSubscriptions(list);
+    res.json(results);
+  } catch (e) {
+    res.status(500).send(e.message || e);
+  }
+});
+
+//Returns members of selected list
+
+router.post("/members", requireAuth, async function(req, res, next) {
+  try {
+    const list = {
+      _id: req.body.listID
+    };
+    const results = await getMembers(list);
+    res.json(results);
+  } catch (e) {
+    res.status(500).send(e.message || e);
+  }
+});
+
+//Returns subscribers of selected list
+
+router.post("/subscribers", requireAuth, async function(req, res, next) {
     try {
-      const loggedInUser = req.user;
-     const list = {
-        members: loggedInUser.userID,
+      const list = {
+        _id: req.body.listID
       };
-      const results=await getMemberships(list);
+      const results = await getSubscribers(list);
       res.json(results);
     } catch (e) {
       res.status(500).send(e.message || e);
     }
   });
 
-  router.get('/subscriptions', requireAuth, async function (req, res, next) {
-   
-    try {
-      const loggedInUser = req.user;
-     const list = {
-        subscribers: loggedInUser.userID,
-      };
-      const results=await getSubscriptions(list);
-      res.json(results);
-    } catch (e) {
-      res.status(500).send(e.message || e);
-    }
-  });
-
-  module.exports = router;
+module.exports = router;
