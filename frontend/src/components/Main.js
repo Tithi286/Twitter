fa2f4c1c420b093f188abb08df27dd5f8e498245
YeshaDave <yeshadave9@gmail.com>
2019-11-26 13:17:56 -0800

    Explore frontend

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -5,6 +5,7 @@ import Login from './login/login';
 import Launchpage from './users/launchpage'
 import UserHome from './userFeed/userHome'
 import Explore from './userFeed/explore'
+import Explore1 from './userFeed/explore1'
 import Messages from './userFeed/messages'
 import Bookmarks from './userFeed/bookmarks'
 import Lists from './userFeed/lists'
@@ -24,6 +25,7 @@ class Main extends Component {
                 <Route path="/launchpage" component={Launchpage} />
                 <Route path="/home" component={UserHome} />
                 <Route path="/explore" component={Explore} />
+                <Route path="/explore1" component={Explore1} />
                 <Route path="/messages" component={Messages} />
                 <Route path="/bookmarks" component={Bookmarks} />
                 <Route path="/lists" component={Lists} />
