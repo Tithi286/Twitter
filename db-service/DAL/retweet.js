bbb1e2027e076714c6102ef67d30697820da6b83
Priya Khadke <priyakhadke15@gmail.com>
2019-11-28 10:48:42 -0800

    added methods to get counts of replies/retweets/likes (using mongo aggregate query)

--- db-service/DAL/retweet.js
+++ db-service/DAL/retweet.js
@@ -7,6 +7,42 @@ const getRetweet = connection => (retweet = {}) => {
         });
     });
 };
+
+const getRetweetCount = connection => tweetIds => {
+    return new Promise((resolve, reject) => {
+        tweetIds = Array.isArray(tweetIds) ? tweetIds : [tweetIds];
+        retweets.aggregate(
+            [
+                { "$match": { "tweetID": { "$in": tweetIds } } },
+                { "$group": { "_id": "$tweetID", "count": { "$sum": 1 } } }
+            ],
+            (err, docs) => {
+                if (err) {
+                    return reject(err);
+                }
+                // return as map something like
+                /*
+                    {
+                        "tweetID1": 2,
+                        "tweetID2": 5,
+                        ...
+                        ...
+                    }
+                */
+                const replyCountMap = tweetIds.reduce((acc, t) => {
+                    acc[t] = 0;
+                    return acc;
+                }, {});
+                docs = JSON.parse(JSON.stringify(docs));
+                docs.forEach(elem => {
+                    replyCountMap[elem._id] = elem.count;
+                });
+                return resolve(replyCountMap);
+            }
+        );
+    });
+};
+
 const saveRetweet = connection => (retweet) => {
     let retweetDoc = new retweets(retweet);
     return new Promise((resolve, reject) => {
@@ -15,7 +51,9 @@ const saveRetweet = connection => (retweet) => {
         })
     });
 };
+
 module.exports = {
     getRetweet,
     saveRetweet,
+    getRetweetCount
 };
