7b5004beb29626a366d1d2b116fd25ab9b92c265
HarshadaJiv <55175861+HarshadaJiv@users.noreply.github.com>
2019-11-30 02:35:19 -0800

    Updated DAL analytics

--- backend/DataAccessLayer/analytics.js
+++ backend/DataAccessLayer/analytics.js
@@ -1,75 +1,166 @@
 const { tweets } = require("./Models/TweetSchema");
-const {profileview }=require("./Models/ProfileViewSchema");
+const { profileview } = require("./Models/ProfileViewSchema");
+const { likes } = require("./Models/LikeSchema");
+const { retweets } = require("./Models/RetweetSchema");
 
 const getTweetViewCount = connection => (query = {}) => {
   return new Promise((resolve, reject) => {
-    tweets.find(query, function(err, docs) {
-      return err ? reject(err) : resolve(docs);
-    });
+    tweets
+      .find(function(err, docs) {
+        return err ? reject(err) : resolve(docs);
+      })
+      .sort({ viewCount: -1 })
+      .limit(10);
   });
 };
 
 const getTweetLikeCount = connection => (query = {}) => {
   return new Promise((resolve, reject) => {
-    tweets.find(query, function(err, docs) {
-      return err ? reject(err) : resolve(docs);
-    });
+    likes
+      .aggregate(
+        [{ $group: { _id: "$tweetID", count: { $sum: 1 } } }],
+        function(err, docs) {
+          return err ? reject(err) : resolve(docs);
+        }
+      )
+      .limit(10);
   });
 };
 
 const getRetweetCount = connection => (query = {}) => {
   return new Promise((resolve, reject) => {
-    tweets.find(query, function(err, docs) {
-      return err ? reject(err) : resolve(docs);
-    });
+    retweets
+      .aggregate(
+        [{ $group: { _id: "$tweetID", count: { $sum: 1 } } }],
+        function(err, docs) {
+          return err ? reject(err) : resolve(docs);
+        }
+      )
+      .limit(10);
   });
 };
 
 const getTweetCountHour = connection => (query = {}) => {
   return new Promise((resolve, reject) => {
-    tweets.find(query, function(err, docs) {
-      return err ? reject(err) : resolve(docs);
-    });
+    tweets.aggregate(
+      [
+        {
+          $group: {
+            _id: {
+              h: { $hour: "$tweetDate" },
+              d: { $dayOfMonth: "$tweetDate" },
+              m: { $month: "$tweetDate" },
+              y: { $year: "$tweetDate" }
+            },
+            count: { $sum: 1 }
+          }
+        },
+        { $project: { tweet: 1, count: 1 } }
+      ],
+      function(err, docs) {
+        return err ? reject(err) : resolve(docs);
+      }
+    );
   });
 };
 const getTweetCountDay = connection => (query = {}) => {
   return new Promise((resolve, reject) => {
-    tweets.find(query, function(err, docs) {
-      return err ? reject(err) : resolve(docs);
-    });
+    tweets.aggregate(
+      [
+        {
+          $group: {
+            _id: {
+              d: { $dayOfMonth: "$tweetDate" },
+              m: { $month: "$tweetDate" },
+              y: { $year: "$tweetDate" }
+            },
+            count: { $sum: 1 }
+          }
+        },
+        { $project: { tweet: 1, count: 1 } }
+      ],
+      function(err, docs) {
+        return err ? reject(err) : resolve(docs);
+      }
+    );
   });
 };
 
 const getTweetCountMonth = connection => (query = {}) => {
   return new Promise((resolve, reject) => {
-    tweets.find(query, function(err, docs) {
-      return err ? reject(err) : resolve(docs);
-    });
+    tweets.aggregate(
+      [
+        {
+          $group: {
+            _id: {
+              m: { $month: "$tweetDate" },
+              y: { $year: "$tweetDate" }
+            },
+            count: { $sum: 1 }
+          }
+        },
+        { $project: { tweet: 1, count: 1 } }
+      ],
+      function(err, docs) {
+        return err ? reject(err) : resolve(docs);
+      }
+    );
   });
 };
 
 const getProfileViewCount = connection => (query = {}) => {
   return new Promise((resolve, reject) => {
-    tweets.find(query, function(err, docs) {
+    const userID = query.userID;
+    console.log(userID);
+    profileview.aggregate(
+      [
+        { $match: { userID: userID } },
+        {
+          $group: {
+            _id: {
+              d: { $dayOfMonth: "$viewDate" },
+              m: { $month: "$viewDate" },
+              y: { $year: "$viewDate" },
+              userID: "$userID"
+            },
+            count: { $sum: 1 }
+          }
+        }
+      ],
+      function(err, docs) {
+        return err ? reject(err) : resolve(docs);
+      }
+    );
+  });
+};
+
+const IncTweetViewCount = connection => (query = {}) => {
+  return new Promise((resolve, reject) => {
+    const tweetID = query.tweetID;
+    tweets.updateOne({ tweetID: tweetID }, { $inc: { viewCount: 1 } }, function(
+      err,
+      docs
+    ) {
       return err ? reject(err) : resolve(docs);
     });
   });
 };
-
-const IncTweetViewCount= connection => (query = {}) => {
-    return new Promise((resolve, reject) => {
-      tweets.find(query, function(err, docs) {
+const IncProfileViewCount = connection => (query = {}) => {
+  return new Promise((resolve, reject) => {
+    const userID = query.userID;
+    profileview.updateOne(
+      { userID: userID, viewDate: Date.now() },
+      {
+        $set: { viewDate: Date.now() },
+        $inc: { viewCount: 1 }
+      },
+      { upsert: true },
+      function(err, docs) {
         return err ? reject(err) : resolve(docs);
-      });
-    });
-  };
-  const IncProfileViewCount = connection => (query = {}) => {
-    return new Promise((resolve, reject) => {
-     profileview.find(query, function(err, docs) {
-        return err ? reject(err) : resolve(docs);
-      });
-    });
-  };
+      }
+    );
+  });
+};
 module.exports = {
   getTweetViewCount,
   getTweetLikeCount,
