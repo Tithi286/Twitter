ce774123c26ad84cb4454fef0f6408cff914f77f
Priya Khadke <priyakhadke15@gmail.com>
2019-12-02 09:48:31 -0800

    One tweet details GET tweets/details API

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ var passport = require('passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var tweetsRouter = require('./routes/tweets');
 var userprofileRouter = require('./routes/userprofile');
 var listsRouter = require('./routes/lists');
 var userfeedRouter = require('./routes/userfeed');
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'uploads')));
 //All Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/tweets', tweetsRouter);
 app.use('/userprofile', userprofileRouter);
 app.use('/lists', listsRouter);
 app.use('/userfeed', userfeedRouter);
