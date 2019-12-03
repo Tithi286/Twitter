4ffd9395a760ceb60e5d0a8c7b869dc295e7a2d3
Priya Khadke <priyakhadke15@gmail.com>
2019-12-03 14:34:59 -0800

    Analytics frontend integration . Added 5 charts , removed sidebar, minor fixes in CSS

--- frontend/src/components/userFeed/analytics.js
+++ frontend/src/components/userFeed/analytics.js
@@ -11,11 +11,124 @@ class analytics extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            tweetview10: [15, 28, 20, 41, 35, 36, 46, 54, 34, 50],
-            tweetview10Xaxis: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sept', 'Oct', 'Nov'],
+            tweetview10: [],
+            tweetview10Xaxis: [],
             tweetlike10: '',
             tweetretweet5: '',
-            tweethourmondaily: ''
+            tweethourcount: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            tweetmonthlycount: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            tweetdailycount: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            profileviewcount: 0,
+        }
+    }
+    async componentDidMount() {
+        let viewtweetCounts = [];
+        let tweetXaxis = [];
+        let tweetliketop10 = [];
+        let retweettop10 = [];
+        let hourcount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        let monthlycount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        let dailycount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+        try {
+            const responses = await Promise.all([
+                fetch(`/analytics/viewcount`, {
+                    method: 'get',
+                    mode: "cors",
+                    redirect: 'follow',
+                    headers: {
+                        'content-type': 'application/json'
+                    }
+                }),
+                fetch(`/analytics/likecount`, {
+                    method: 'get',
+                    mode: "cors",
+                    redirect: 'follow',
+                    headers: {
+                        'content-type': 'application/json'
+                    }
+                }),
+                fetch(`/analytics/retweetcount`, {
+                    method: 'get',
+                    mode: "cors",
+                    redirect: 'follow',
+                    headers: {
+                        'content-type': 'application/json'
+                    }
+                }),
+                fetch(`/analytics/hourcount`, {
+                    method: 'get',
+                    mode: "cors",
+                    redirect: 'follow',
+                    headers: {
+                        'content-type': 'application/json'
+                    }
+                }),
+                fetch(`/analytics/daycount`, {
+                    method: 'get',
+                    mode: "cors",
+                    redirect: 'follow',
+                    headers: {
+                        'content-type': 'application/json'
+                    }
+                }),
+                fetch(`/analytics/monthcount`, {
+                    method: 'get',
+                    mode: "cors",
+                    redirect: 'follow',
+                    headers: {
+                        'content-type': 'application/json'
+                    }
+                }),
+                fetch(`/analytics/profileviewcount`, {
+                    method: 'get',
+                    mode: "cors",
+                    redirect: 'follow',
+                    headers: {
+                        'content-type': 'application/json'
+                    }
+                })
+            ]);
+            const [res1, res2, res3, res4, res5, res6, res7] = await Promise.all(responses.map(r => r.json()));
+
+            res1.forEach(obj => {
+                viewtweetCounts.push(obj.viewCount);
+                tweetXaxis.push(obj.tweetID);
+            });
+
+            res2.forEach(obj => {
+                tweetliketop10.push({ y: obj.count });
+            });
+
+            res3.forEach(obj => {
+                retweettop10.push(obj.count);
+            });
+
+            res4.forEach(cnt => {
+                hourcount[cnt._id.h] = cnt.count;
+            });
+
+            res5.forEach(cnt => {
+                dailycount[cnt._id.d] = cnt.count;
+            });
+
+            res6.forEach(cnt => {
+                monthlycount[Number(cnt._id.m) - 1] = cnt.count
+            });
+
+            this.setState({
+                tweetview10: viewtweetCounts,
+                tweetview10Xaxis: tweetXaxis,
+                tweetlike10: tweetliketop10,
+                tweetretweet5: retweettop10,
+                tweethourcount: hourcount,
+                tweetdailycount: dailycount,
+                tweetmonthlycount: monthlycount,
+                profileviewcount: res7[0].count,
+            });
+        }
+        catch (e) {
+            this.setState({ msg: e.message || e });
         }
     }
     render() {
@@ -147,293 +260,191 @@ class analytics extends Component {
                 data: this.state.tweetview10
             }]
         };
-        const options = {
+        const top10like = {
             chart: {
                 type: "pie"
             },
+            title: {
+                text: 'Top 10 liked tweets',
+            },
             series: [{
-                data: [
-                    {
-                        y: 100
-                    },
-                    {
-                        y: 50
-                    }
-                ]
+                data: this.state.tweetlike10
             }]
         };
-        const timechart = {
+        const top5retweet = {
             chart: {
-                rangeSelector: {
-                    selected: 4
-                },
-                backgroundColor: {
-                    linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
-                    stops: [
-                        [0, '#2a2a2b'],
-                        [1, '#3e3e40']
-                    ]
-                },
-                style: {
-                    fontFamily: '\'Unica One\', sans-serif'
-                },
-                plotBorderColor: '#606063'
+                type: "pie"
             },
             title: {
-                text: 'number of tweets',
-                style: {
-                    color: '#E0E0E3',
-                    textTransform: 'uppercase',
-                    fontSize: '20px'
-                }
-            },
-            subtitle: {
-                text: '(hourly/daily/monthly)',
-                style: {
-                    color: '#E0E0E3',
-                    textTransform: 'uppercase'
-                }
+                text: 'Top 5 retweets',
             },
             series: [{
-                data: [[Date.UTC(2013, 5, 2), 0.7695],
-                [Date.UTC(2013, 5, 3), 0.7648],
-                [Date.UTC(2013, 5, 24), 0.7623],]
-            }],
-            xAxis: {
-                type: 'datetime',
-                labels: {
-                    format: '{value:%d-%m-%Y}',
-                    style: {
-                        color: '#E0E0E3'
-                    },
-                    min: Date.UTC(2010, 8, 1),
-                    max: Date.UTC(2019, 8, 1)
-                },
-                gridLineColor: '#707073',
-                lineColor: '#707073',
-                minorGridLineColor: '#505053',
-                tickColor: '#707073',
-                title: {
-                    style: {
-                        color: '#A0A0A3'
-                    }
-                }
-            },
-            yAxis: {
-                gridLineColor: '#707073',
-                labels: {
-                    style: {
-                        color: '#E0E0E3'
-                    }
-                },
-                lineColor: '#707073',
-                minorGridLineColor: '#505053',
-                tickColor: '#707073',
-                tickWidth: 1,
-                title: {
-                    style: {
-                        color: '#A0A0A3'
-                    }
-                }
-            },
-            tooltip: {
-                backgroundColor: 'rgba(0, 0, 0, 0.85)',
-                style: {
-                    color: '#F0F0F0'
-                }
-            },
-            plotOptions: {
-                series: {
-                    dataLabels: {
-                        color: '#F0F0F3',
-                        style: {
-                            fontSize: '13px'
-                        }
-                    },
-                    marker: {
-                        lineColor: '#333'
-                    }
-                },
-                boxplot: {
-                    fillColor: '#505053'
-                },
-                candlestick: {
-                    lineColor: 'white'
-                },
-                errorbar: {
-                    color: 'white'
-                }
-            },
-            legend: {
-                backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                itemStyle: {
-                    color: '#E0E0E3'
-                },
-                itemHoverStyle: {
-                    color: '#FFF'
-                },
-                itemHiddenStyle: {
-                    color: '#606063'
-                },
-                title: {
-                    style: {
-                        color: '#C0C0C0'
-                    }
-                }
-            },
-            labels: {
-                style: {
-                    color: '#707073'
-                }
-            },
-            drilldown: {
-                activeAxisLabelStyle: {
-                    color: '#F0F0F3'
-                },
-                activeDataLabelStyle: {
-                    color: '#F0F0F3'
-                }
-            },
-            navigation: {
-                buttonOptions: {
-                    symbolStroke: '#DDDDDD',
-                    theme: {
-                        fill: '#505053'
-                    }
-                }
-            },
-            // scroll charts
-            rangeSelector: {
-                buttonTheme: {
-                    fill: '#505053',
-                    stroke: '#000000',
-                    style: {
-                        color: '#CCC'
-                    },
-                    states: {
-                        hover: {
-                            fill: '#707073',
-                            stroke: '#000000',
-                            style: {
-                                color: 'white'
-                            }
-                        },
-                        select: {
-                            fill: '#000003',
-                            stroke: '#000000',
-                            style: {
-                                color: 'white'
-                            }
-                        }
-                    }
-                },
-                inputBoxBorderColor: '#505053',
-                inputStyle: {
-                    backgroundColor: '#333',
-                    color: 'silver'
-                },
-                labelStyle: {
-                    color: 'silver'
-                }
-            },
-            navigator: {
-                handles: {
-                    backgroundColor: '#666',
-                    borderColor: '#AAA'
-                },
-                outlineColor: '#CCC',
-                maskFill: 'rgba(255,255,255,0.1)',
-                series: {
-                    color: '#7798BF',
-                    lineColor: '#A6C7ED'
-                },
-                xAxis: {
-                    gridLineColor: '#505053'
-                }
-            },
-            scrollbar: {
-                barBackgroundColor: '#808083',
-                barBorderColor: '#808083',
-                buttonArrowColor: '#CCC',
-                buttonBackgroundColor: '#606063',
-                buttonBorderColor: '#606063',
-                rifleColor: '#FFF',
-                trackBackgroundColor: '#404043',
-                trackBorderColor: '#404043'
-            }
+                data: this.state.tweetretweet5
+            }]
         };
-        var gaugeOptions = {
-
+        var hourly = {
             chart: {
-                type: 'solidgauge'
+                type: 'column'
             },
-
-            title: null,
-
-            pane: {
-                center: ['50%', '85%'],
-                size: '140%',
-                startAngle: -90,
-                endAngle: 90,
-                background: {
-                    backgroundColor:
-                        Highcharts.defaultOptions.legend.backgroundColor || '#EEE',
-                    innerRadius: '60%',
-                    outerRadius: '100%',
-                    shape: 'arc'
-                }
+            title: {
+                text: 'Tweets Statistics'
             },
-
-            tooltip: {
-                enabled: false
+            subtitle: {
+                text: 'Hourly Tweet Count'
             },
-
-            // the value axis
+            xAxis: {
+                categories: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+                crosshair: true
+            },
             yAxis: {
-                stops: [
-                    [0.1, '#55BF3B'], // green
-                    [0.5, '#DDDF0D'], // yellow
-                    [0.9, '#DF5353'] // red
-                ],
-                lineWidth: 0,
-                minorTickInterval: null,
-                tickAmount: 2,
+                min: 0,
                 title: {
-                    y: -70
-                },
-                labels: {
-                    y: 16
+                    text: 'Tweet Count'
                 }
             },
-
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y}</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
             plotOptions: {
-                solidgauge: {
-                    dataLabels: {
-                        y: 5,
-                        borderWidth: 0,
-                        useHTML: true
-                    }
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
                 }
-            }
+            },
+            series: [
+                {
+                    name: 'Hourly',
+                    data: this.state.tweethourcount
+                },
+                // {
+                //     name: 'Daily',
+                //     data: this.state.tweetdailycount
+                // },
+                // {
+                //     name: 'Monthly',
+                //     data: this.state.tweetmonthlycount
+                // }
+            ]
         };
+        var daily = {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Tweets Statistics'
+            },
+            subtitle: {
+                text: 'Daily Tweet Count'
+            },
+            xAxis: {
+                // categories: [
+                //     'Jan'
+                // ],
+                crosshair: true
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Tweet Count'
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y}</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series: [
 
+                {
+                    name: 'Daily',
+                    data: this.state.tweetdailycount
+                },
+                // {
+                //     name: 'Monthly',
+                //     data: this.state.tweetmonthlycount
+                // }
+            ]
+        };
+        var monthly = {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Tweets Statistics'
+            },
+            subtitle: {
+                text: 'Monthly Tweet Counts'
+            },
+            xAxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                crosshair: true
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Tweet Count'
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y}</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series: [
+                {
+                    name: 'Monthly',
+                    data: this.state.tweetmonthlycount
+                }
+            ]
+        };
         return (
             <div className="container-flex">
-            {redirectVar}
-                <Navbar/>
-                <div className="col-md-9 feed">
-                    <div class="home-font">Analytics</div>
+                <div className="col-md-12 feed">
+                    <div style={{ width: "60%", margin: "0 auto", textAlign: "center" }}>
+                        <h3>My Profile View Count         <span class="badge badge-primary">  {this.state.profileviewcount}</span></h3>
+                    </div>
                     <HighchartsReact highcharts={Highcharts} options={tweetview10} />
-
+                    <div class="col-md-6 feed">
+                        <PieChart highcharts={Highcharts} options={top5retweet} />
+                    </div>
+                    <div className="col-md-6 feed">
+                        <PieChart highcharts={Highcharts} options={top10like} />
+                    </div>
+                    <div className="col-md-12 feed">
+                        <div className="col-md-4 feed">
+                            <HighchartsReact highcharts={Highcharts} options={hourly} />
+                        </div>
+                        <div className="col-md-4 feed">
+                            <HighchartsReact highcharts={Highcharts} options={daily} />
+                        </div>
+                        <div className="col-md-4 feed">
+                            <HighchartsReact highcharts={Highcharts} options={monthly} />
+                        </div>
+                    </div>
                 </div>
-                <div className="col-md-4 feed">
-                    <PieChart highcharts={Highcharts} options={options} />
-                </div>
-
-                <div className="col-md-4 feed">
-                    <PieChart highcharts={Highcharts} options={options} />
-                </div>
-
             </div>
         )
     }
