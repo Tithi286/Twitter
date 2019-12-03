0ed94410736a4b43bf8f5483f85b2ffbe5b8e6df
Harshada Jivane <harshadajivane@Vireshs-MacBook-Pro.local>
2019-12-02 16:54:20 -0800

    fixed get messages

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -18,16 +18,18 @@ router.get("/", requireAuth, async function (req, res, next) {
     const messages = {
       senderID: loggedInUser.userID
     };
-    results = await simulateRequestOverKafka("getMessages", messages);
+    console.log(messages)
+    results = await simulateRequestOverKafka("getOwnMessages", messages);
     results.forEach(retwt => {
       receiverID.push(retwt.receiverID);
     });
 
     quoted = "'" + receiverID.join("','") + "'";
     const user = { usersID: [quoted] };
-
+    console.log(results)
     let chatRes = await simulateRequestOverKafka("getUsers", user);
     res.json(chatRes.results)
+    console.log(chatRes)
   } catch (e) {
     res.status(500).send(e.message || e);
   }
